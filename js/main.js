905b813e927427011951ab3dad046739103a0129
codechappie <davidproyecto01@gmail.com>
2020-01-12 22:16:24 -0500

    avance de menu

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 window.onload = function() {
   var cloneMenu = document.getElementById("menu").innerHTML;
-  console.log(cloneMenu);
   var menuContent = document.getElementById("mini__menu");
   var menuResponsive = document.getElementById("ident");
-  console.log(menuResponsive);
   var buttonMenu = document.getElementById("buttonmenu");
   var buttonClose = document.getElementById("buttonclose");
 
   buttonMenu.addEventListener("click", function toogle() {
     menuContent.innerHTML = cloneMenu;
-    console.log(menuResponsive);
     menuResponsive.classList.toggle("show");
   });
   buttonClose.addEventListener("click", function closeMenu() {
-    console.log(menuResponsive);
     menuResponsive.classList.remove("show");
   });
+
+  var myNav = document.getElementById('navbar');
+window.onscroll = function () { 
+    if (window.scrollY >= 120 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
+  
+
 };
